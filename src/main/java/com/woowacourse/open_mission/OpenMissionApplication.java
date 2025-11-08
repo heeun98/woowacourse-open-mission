a32956066bcb8e4a744a12bb56c9957164445448
heeun98 <phu98@naver.com>
2025-11-08 19:30:55 +0900

    docs: README.md 에 단계별 구현과 목적 작성

---
+++ src/main/java/com/woowacourse/open_mission/OpenMissionApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.open_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenMissionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/woowacourse/open_mission/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.woowacourse.open_mission;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpenMissionApplication.class);
+	}
+
+}
