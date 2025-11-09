b2ae1727ad2a4ca1db2b790b73ef0665d50b5061
heeun98 <phu98@naver.com>
2025-11-10 00:47:21 +0900

    feat: 당첨 통계를 저장하는 LottoCount 구현

---
+++ src/main/java/com/woowacourse/open_mission/LottoCount.java
@@ -0,0 +1,31 @@
+package com.woowacourse.open_mission;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoCount {
+
+    private Map<String, Integer> count;
+
+    public LottoCount() {
+        count = new HashMap<>();
+        initMap();
+    }
+
+    private void initMap() {
+        for (String message : Rank.getAllMessage()) {
+            count.put(message, 0);
+        }
+    }
+
+    public void addCount(int matchingCount, boolean matchBonus) {
+
+        String message = Rank.getMatchMessage(matchingCount, matchBonus);
+        if (message != null) {
+            count.put(message, count.get(message) + 1);
+        }
+
+    }
+
+
+}
