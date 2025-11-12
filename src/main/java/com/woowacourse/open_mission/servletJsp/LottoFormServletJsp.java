6605c0e340959bd06971de54ea669c3d0b017a7a
heeun98 <phu98@naver.com>
2025-11-12 15:56:00 +0900

    feat: 로또 시작하기 서블릿, JSP 구현

---
+++ src/main/java/com/woowacourse/open_mission/servletJsp/LottoFormServletJsp.java
@@ -0,0 +1,27 @@
+package com.woowacourse.open_mission.servletJsp;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+@WebServlet(name = "LottoFormServletJsp", value = "/servlet/jsp/lotto")
+public class LottoFormServletJsp extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("로또 시작하기 폼 서블릿 호출");
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/lott-start.jsp");
+
+        dispatcher.forward(request, response);
+
+    }
+}
