c0e3340932a7b472fb408a20a38aa487054eddae
heeun98 <phu98@naver.com>
2025-11-12 15:56:30 +0900

    fix: LottoFormServlet 클래스명 수정

--- src/main/java/com/woowacourse/open_mission/servletJsp/LottoMainServletJsp.java
+++ src/main/java/com/woowacourse/open_mission/servletJsp/LottoMainServletJsp.java
@@ -1,19 +1,25 @@
 package com.woowacourse.open_mission.servletJsp;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 
-@WebServlet(name = "LottoMainServlet", urlPatterns = "/servlet")
-public class LottoMainServlet extends HttpServlet {
+
+@Slf4j
+@WebServlet(name = "LottoMainJspServlet", urlPatterns = "/servlet/jsp")
+public class LottoMainServletJsp extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/views/lotto-main.jsp")
-                .forward(request, response);
+        log.info("메인 서블릿 호출");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/lotto-main.jsp");
+                
+        
+        dispatcher.forward(request, response);
     }
 }
