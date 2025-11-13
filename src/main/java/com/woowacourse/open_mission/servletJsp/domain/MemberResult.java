d5a862dcb47bb90ee8b88fba549156388433c0d8
heeun98 <phu98@naver.com>
2025-11-14 01:57:14 +0900

    feat(MemberResult): 결과를 담는 Map 초기화 메서드 구현

--- src/main/java/com/woowacourse/open_mission/servletJsp/domain/MemberResult.java
+++ src/main/java/com/woowacourse/open_mission/servletJsp/domain/MemberResult.java
@@ -1,34 +1,48 @@
 package com.woowacourse.open_mission.servletJsp.domain;
 
+import com.woowacourse.open_mission.Rank;
+
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MemberResult {
 
+
     private final String name;
     private final LocalDate createdAt;
-    private Map<String, Integer> result;
+    private Map<String, Integer> result;// key: 6개 일치 (2,000,000,000원) value : 개수
     private MemberRepository memberRepository;
 
 
-    public MemberResult(String name, LocalDate createdAt) {
+    public MemberResult(String name) {
         this.name = name;
-        this.createdAt = createdAt;
+        this.createdAt = LocalDate.now();
+        result = new HashMap<>();
         memberRepository = MemberRepository.getInstance();
     }
 
+    public void initResult() {
+        Rank.getAllMessages().stream()
+                .forEach(s -> result.put(s, 0));
+    }
 
-    public void generateCount(String name) {
+    public void addResultCount(String message) {
+        result.put(message, result.get(message) + 1);
+    }
 
-        List<LottoTickets> lottoTickets = memberRepository.getLottoTicketsByName(name);
 
+    public String getName() {
+        return name;
+    }
 
-        lottoTickets.stream()
-                .forEach(lottoTicket -> {
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
 
-                });
-
+    public Map<String, Integer> getResult() {
+        return result;
     }
 
 }
