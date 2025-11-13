7c62c03e4596929b047d177646d7be310abdfb55
hyojin425 <gywls5184@gmail.com>
2025-11-14 03:08:11 +0900

    fix(MemberResultRepository): 결과 Map으로 저장하도록 변경
    - key: resultId, value: result

--- src/main/java/com/woowacourse/open_mission/servletJsp/domain/MemberResultRepository.java
+++ src/main/java/com/woowacourse/open_mission/servletJsp/domain/MemberResultRepository.java
@@ -1,28 +1,33 @@
 package com.woowacourse.open_mission.servletJsp.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 public class MemberResultRepository {
 
     private static final MemberResultRepository memberResultRepository = new MemberResultRepository();
+    private static Long resultId = 1L;
 
-    List<MemberResult> memberResultList = new ArrayList<>();
+    Map<Long, MemberResult> memberResultMap= new HashMap<>();
 
     public static MemberResultRepository getInstance() {
         return memberResultRepository;
     }
 
 
-
     public void addMemberResult(MemberResult memberResult) {
-        memberResultList.add(memberResult);
+        memberResultMap.put(resultId, memberResult);
+        resultId++;
     }
 
+    public Map<Long, MemberResult> getMemberResultList() {
+        return memberResultMap;
+    }
 
-
-
-
-
-
+    public MemberResult getMemberResultById(Long resultId) {
+        return memberResultMap.get(resultId);
+    }
 }
