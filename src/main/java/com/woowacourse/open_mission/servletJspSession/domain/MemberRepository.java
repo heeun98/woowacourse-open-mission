d70213c0cd2399bad44002f414281a1fc054637e
hyojin425 <gywls5184@gmail.com>
2025-11-17 00:04:54 +0900

    feat(MemberRepository): memberRepository 추가

--- src/main/java/com/woowacourse/open_mission/servletJspSession/domain/MemberRepository.java
+++ src/main/java/com/woowacourse/open_mission/servletJspSession/domain/MemberRepository.java
@@ -3,15 +3,23 @@ package com.woowacourse.open_mission.servletJspSession.domain;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 public class MemberRepository {
 
-    private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L;
+    private static Map<Long, Member> store = new ConcurrentHashMap<Long, Member>();
+    private static AtomicLong sequence = new AtomicLong(0);
+
+    private static final MemberRepository memberRepository = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return memberRepository;
+    }
 
     public Member save(Member member) {
-        member.setId(++sequence);
+        member.setId(sequence.incrementAndGet());
         log.info("save: member={}", member);
         store.put(member.getId(), member);
         return member;
