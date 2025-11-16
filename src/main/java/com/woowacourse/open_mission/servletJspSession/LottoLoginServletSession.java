a51a3922880c7253ca8ced684c0ff7363d9b0311
hyojin425 <gywls5184@gmail.com>
2025-11-17 00:05:24 +0900

    feat: 세션 기반 로그인 기능 구현

--- src/main/java/com/woowacourse/open_mission/servletJspSession/LottoLoginServletSession.java
+++ src/main/java/com/woowacourse/open_mission/servletJspSession/LottoLoginServletSession.java
@@ -1,22 +1,44 @@
 package com.woowacourse.open_mission.servletJspSession;
 
-import jakarta.servlet.RequestDispatcher;
+import com.woowacourse.open_mission.servletJspSession.domain.Member;
+import com.woowacourse.open_mission.servletJspSession.domain.MemberRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "LottoLoginServletSession", value = "/v3/servlet/jsp/login")
+@Slf4j
 public class LottoLoginServletSession extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.setAttribute();
+    MemberRepository memberRepository = MemberRepository.getInstance();
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/viewsV2/lotto-login.jsp");
-        dispatcher.forward(request, response);
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String loginId = request.getParameter("loginId");
+        String password = request.getParameter("password");
+
+        Optional<Member> member1 = memberRepository.findByLoginId("gywls5184");
+        log.info(member1.toString());
+
+        Optional<Member> optionalMember = memberRepository.findByLoginId(loginId);
+
+        if (optionalMember.isEmpty()) {
+            response.sendRedirect("/v3/servlet/jsp/login-form");
+            return;
+        }
+
+        Member member = optionalMember.get();
+        if (!member.getPassword().equals(password)) {
+            response.sendRedirect("/v3/servlet/jsp/login-form");
+            return;
+        }
+
+        response.sendRedirect("/v3/servlet/jsp");
     }
 }
