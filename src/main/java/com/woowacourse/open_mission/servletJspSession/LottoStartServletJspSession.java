09e3e8dc5b346637352ab9e7893036b561e026ef
heeun98 <phu98@naver.com>
2025-11-17 21:52:17 +0900

    refactor: 로또 시작하기 서블릿 컨트롤러로 수정

--- src/main/java/com/woowacourse/open_mission/servletJspSession/LottoStartServletJspSession.java
+++ src/main/java/com/woowacourse/open_mission/servletJspSession/LottoStartServletJspSession.java
@@ -1,33 +1,39 @@
 package com.woowacourse.open_mission.servletJspSession;
 
 import com.woowacourse.open_mission.domain.MemberTicketsRepository;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import com.woowacourse.open_mission.servletJspSession.frontController.LottoController;
+import com.woowacourse.open_mission.servletJspSession.frontController.Session;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
-@WebServlet(name = "LottoStartServletJspSession", value = "/v3/servlet/jsp/lotto")
-public class LottoStartServletJspSession extends HttpServlet {
+//@WebServlet(name = "LottoStartServletJspSession", value = "/v3/servlet/jsp/lotto")
+public class LottoStartServletJspSession implements LottoController {
 
 
     private final MemberTicketsRepository memberTicketsRepository = MemberTicketsRepository.getInstance();
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.info("LottoStartServletJsp 호출");
-
-
+    public String process(Map<String, String> paramMap, Map<String, Object> model, Session session) {
         int totalBuyCount = memberTicketsRepository.getTotalBuyCount();
-        request.setAttribute("totalBuyCount", totalBuyCount);
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/viewsV2/lotto-start.jsp");
+        model.put("totalBuyCount", totalBuyCount);
 
-        dispatcher.forward(request, response);
-
+        return "lotto-start";
     }
+
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        log.info("LottoStartServletJsp 호출");
+//
+//
+//        int totalBuyCount = memberTicketsRepository.getTotalBuyCount();
+//        request.setAttribute("totalBuyCount", totalBuyCount);
+//
+//        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/viewsV2/lotto-start.jsp");
+//
+//        dispatcher.forward(request, response);
+//
+//    }
 }
